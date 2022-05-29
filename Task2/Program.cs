41031cdd25f22e5669ede9d924969956eb592873
Vladyslav <rudniev.csdev@gmail.com>
2022-05-29 17:39:02 +0300

    .

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,50 +1,44 @@
 ﻿using System;
 using System.Threading;
+using System.Text;
 
 namespace Task2
 {
     class Program
     {
-        static AutoResetEvent auto = new AutoResetEvent(false);
-        static bool flag = false;
+        // Аргумент:
+        // false - установка в несигнальное состояние.
+        static AutoResetEvent auto = new (false);
+
         static void Main()
         {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Thread thread = new Thread(Function);
-            thread.Start();
+            Console.OutputEncoding = Encoding.Unicode;
+            new Thread(Function1).Start();
+            new Thread(Function2).Start();
 
-            Thread.Sleep(1000);
+            Thread.Sleep(500);  // Дадим время запуститься вторичным потокам.
 
-            //Console.WriteLine("Нажмите на любую клавишу для перевода AutoResetEvent в сигнальное состояние.\n");
-            //Console.ReadKey();
-            if (flag)
-            {
-                Thread.Sleep(1);
+            Console.WriteLine("Нажмите на любую клавишу для перевода ManualResetEvent в сигнальное состояние.\n");
+            Console.ReadKey();
+            for (int i = 0; i < 2; i++)
                 auto.Set();
-            }
 
-            Thread.Sleep(1000);
-
-            //Console.WriteLine("Нажмите на любую клавишу для перевода AutoResetEvent в сигнальное состояние.\n");
-            //Console.ReadKey();
-            if (flag)
-            {
-                Thread.Sleep(1);
-                auto.Set();
-            }
+            // Delay
+            Console.ReadKey();
         }
 
-        static void Function()
+        static void Function1()
         {
-            Console.WriteLine("Красный свет");
-            flag = true;
-            auto.WaitOne(); 
+            Console.WriteLine("Поток 1 запущен и ожидает сигнала.");
+            auto.WaitOne(); // Остановка выполнения вторичного потока 1.
+            Console.WriteLine("Поток 1 завершается.");
+        }
 
-            Console.WriteLine("Желтый");
-            flag = true;
-            auto.WaitOne(); 
-
-            Console.WriteLine("Зеленый");
+        static void Function2()
+        {
+            Console.WriteLine("Поток 2 запущен и ожидает сигнала.");
+            auto.WaitOne(); // Остановка выполнения вторичного потока 2.
+            Console.WriteLine("Поток 2 завершается.");
         }
     }
 }
